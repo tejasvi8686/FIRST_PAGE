c0c0d0e88106fc4d403cac40eeeede834c4c052b
tejasvi raj <tejasviraj8686@gmail.com>
2023-10-28 17:20:40 +0530

    added

--- src/components/DrawerComp.jsx
+++ src/components/DrawerComp.jsx
@@ -5,32 +5,64 @@ import {
   List,
   ListItemButton,
   ListItemIcon,
+  Tabs,
+  Tab,
+  Menu,
+  MenuItem,
+  Stack
 } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const DrawerComp = () => {
-  const PAGES = [
-    "Home",
-    "Courses",
-    "Videos",
-    "Contact Us",
-    "About Us",
-    "Test Series",
-    "Study Material",
-    "Blog",
-  ];
   const [openDrawer, setOpenDrawer] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null); // Added state for the anchor element
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget); // Open the menu
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null); // Close the menu
+  };
+
   return (
     <React.Fragment>
       <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
         <List>
-          {PAGES.map((page, index) => (
-            <ListItemButton key={index}>
-              <ListItemIcon>
-                <ListItemIcon>{page}</ListItemIcon>
-              </ListItemIcon>
-            </ListItemButton>
-          ))}
+          <ListItemButton>
+            <ListItemIcon>
+              <Stack direction="column">
+                <Tab label="Home" />
+                <Tab
+                  label="Courses"
+                  aria-controls="courses-menu"
+                  aria-haspopup="true"
+                  onClick={handleOpenMenu}
+                  endIcon={<ArrowDropDownIcon />}
+                />
+                <Menu
+                  id="courses-menu"
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleCloseMenu}
+                >
+                  <MenuItem>HTML & CSS</MenuItem>
+                  <MenuItem>Python</MenuItem>
+                  <MenuItem>Java</MenuItem>
+                  <MenuItem>Power Point</MenuItem>
+                  <MenuItem>Ms Office</MenuItem>
+                </Menu>
+                <Tab label="Videos" />
+                <Tab label="Contact Us" />
+                <Tab label="About Us" />
+                <Tab label="Test Series" />
+                <Tab label="Study Material" />
+                <Tab label="Student Login" />
+                <Tab label="Blog" />
+              </Stack>
+            </ListItemIcon>
+          </ListItemButton>
         </List>
       </Drawer>
       <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,29 +10,16 @@ import {
   Typography,
 } from "@mui/material";
 import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import DrawerComp from "./DrawerComp";
 
 const Navbar = () => {
-  const PAGES = [
-    "Home",
-    "Courses",
-    "Videos",
-    "Contact Us",
-    "About Us",
-    "Test Series",
-    "Study Material",
-    "Blog",
-  ];
   const theme = useTheme();
   console.log(theme);
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   console.log(isMatch);
-
-  const [value, setValue] = useState();
 
   const [anchorEl, setAnchorEl] = useState(null);
 
@@ -47,58 +34,133 @@ const Navbar = () => {
   return (
     <React.Fragment>
       <AppBar position="static" sx={{ bgcolor: "#202C45" }}>
-        <Container>
-          <Toolbar>
-            {isMatch ? (
-              <Box>
-                <Typography></Typography>
-                <DrawerComp />
-              </Box>
-            ) : (
-              <Box>
-                <Tabs
-                  textColor="white"
-                  value={value}
-                  onChange={(e, value) => setValue(value)}
+        <Toolbar sx={{ height: "90px" }}>
+          {isMatch ? (
+            <Box>
+              <Typography></Typography>
+              <DrawerComp />
+            </Box>
+          ) : (
+            <Box >
+              <Tabs textColor="white">
+                <Tab
+                  label="Home"
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Tab
+                  label="Courses"
+                  aria-controls="courses-menu"
+                  aria-haspopup="true"
+                  onClick={handleOpenMenu}
+                  endIcon={<ArrowDropDownIcon />}
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Menu
+                  id="courses-menu"
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleCloseMenu}
                 >
-                  {}
-                  <Tab label="Home" />
-                  <Tab
-                    label="Courses"
-                    aria-controls="courses-menu"
-                    aria-haspopup="true"
-                    onClick={handleOpenMenu}
-                    endIcon={<ArrowDropDownIcon />}
-                  />
-                  <Menu
-                    id="courses-menu"
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={handleCloseMenu}
-                  >
-                    <MenuItem>HTML & CSS</MenuItem>
-                    <MenuItem>Python</MenuItem>
-                    <MenuItem>Java</MenuItem>
-                    <MenuItem>Power Point</MenuItem>
-                    <MenuItem>Ms Office</MenuItem>
-                  </Menu>
+                  <MenuItem>HTML & CSS</MenuItem>
+                  <MenuItem>Python</MenuItem>
+                  <MenuItem>Java</MenuItem>
+                  <MenuItem>Power Point</MenuItem>
+                  <MenuItem>Ms Office</MenuItem>
+                </Menu>
 
-                  <Tab label="Videos" />
-                  <Tab label="Contact Us" />
-                  <Tab label="About Us" />
-                  <Tab label="Test Series " />
-                  <Tab label="Study Material" />
-                  <Tab label="Student Login" />
-                  <Tab label="Blog" />
-                </Tabs>
-              </Box>
-            )}
+                <Tab
+                  label="Videos"
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Tab
+                  label="Contact Us"
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Tab
+                  label="About Us"
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Tab
+                  label="Test Series "
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Tab
+                  label="Study Material"
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Tab
+                  label="Student Login"
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+                <Tab
+                  label="Blog"
+                  sx={{
+                    bgcolor: "#24497D",
+                    marginRight: "8px",
+                    color: "#fff",
+                    borderRadius :"50px",
+                    "&:hover": { bgcolor: "#fff", color: "#202C45" },
+                  }}
+                />
+              </Tabs>
+            </Box>
+          )}
 
-            <Button sx={{ marginLeft: "auto" }} variant="contained">
-              NEW BATCH
-            </Button>
-          </Toolbar>
-        </Container>
+          <Button
+            sx={{ marginLeft: "auto", padding: "20px" }}
+            variant="contained"
+          >
+            NEW BATCH
+          </Button>
+        </Toolbar>
       </AppBar>
     </React.Fragment>
   );
